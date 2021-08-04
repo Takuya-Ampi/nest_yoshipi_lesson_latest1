0a97029c13f70a30758cee2f75b3d6f2a968fc55
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-05 07:49:03 +0900

    fix: pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  findAll() {
+    return this.usersService.findAll()
+  }
+  // @Get(':id')
+  // findOne(@Param('id', new ParseIntPipe()) id: number) {
+  //   return id
+  // }
+  
+  @Get(':id')
+  @UsePipes(new ParseIntPipe())
+  findOne(@Param('id') id: number) {
+    return typeof id
+  }
   @Post()
-  create(@Body() createUserDto: CreateUserDto): CreateUserDto {
-    return createUserDto
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto)
   }
 }
